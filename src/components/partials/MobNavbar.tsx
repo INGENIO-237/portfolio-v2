6655c696107dac09c10120d89738bf7e56423deb
INGENIO <ingeniokhalif@gmail.com>
2024-09-22 10:38:08 +0100

    handle themes - milestone 1

--- src/components/partials/MobNavbar.tsx
+++ src/components/partials/MobNavbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AlignRight } from "lucide-react";
+import { AlignRight, Moon, Sun } from "lucide-react";
 import { useState } from "react";
 import { Sheet, SheetContent } from "../ui/sheet";
 import { navLinks } from "./nav.utils";
@@ -8,7 +8,12 @@ import Link from "next/link";
 import { cn, getKey } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
-export default function MobNavbar() {
+type Props = {
+  theme: "light" | "dark";
+  setTheme: () => void;
+};
+
+export default function MobNavbar({ theme, setTheme }: Props) {
   const [showSheet, setShowSheet] = useState(false);
 
   const toggleSheet = () => {
@@ -19,7 +24,24 @@ export default function MobNavbar() {
 
   return (
     <nav className="md:hidden">
-      <AlignRight size={25} onClick={toggleSheet} />
+      <div className="flex gap-5">
+        {theme === "light" ? (
+          <Sun
+            size={25}
+            className="cursor-pointer"
+            color="gray"
+            onClick={() => setTheme()}
+          />
+        ) : (
+          <Moon
+            size={25}
+            className="cursor-pointer"
+            color="gray"
+            onClick={() => setTheme()}
+          />
+        )}
+        <AlignRight size={25} onClick={toggleSheet} />
+      </div>
 
       <Sheet open={showSheet} onOpenChange={toggleSheet}>
         <SheetContent className="bg-[#FEFEFE] z-[100]">
@@ -31,7 +53,8 @@ export default function MobNavbar() {
                   key={getKey()}
                   className={cn(
                     "text-lg capitalize transition duration-500",
-                    pathname === link.path && "border-l-2 pl-2 border-[#2a9d8f] text-[#2a9d8f]",
+                    pathname === link.path &&
+                      "border-l-2 pl-2 border-[#2a9d8f] text-[#2a9d8f]",
                     "hover:border-l-2 hover:pl-2 hover:border-[#2a9d8f]"
                   )}
                   onClick={toggleSheet}
--- src/components/partials/Navbar.tsx
+++ src/components/partials/Navbar.tsx
@@ -4,12 +4,18 @@ import Link from "next/link";
 import { navLinks } from "./nav.utils";
 import { cn, getKey } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import { Moon, Sun } from "lucide-react";
 
-export default function Navbar() {
+type Props = {
+  theme: "light" | "dark";
+  setTheme: () => void;
+};
+
+export default function Navbar({ theme, setTheme }: Props) {
   const pathname = usePathname();
 
   return (
-    <div className="hidden md:block">
+    <div className="hidden md:flex md:gap-10 md:items-center">
       <ul className="flex gap-5">
         {navLinks.map((link) => {
           return (
@@ -18,9 +24,9 @@ export default function Navbar() {
               key={getKey()}
               className={cn(
                 "text-lg capitalize",
-                pathname == link.path && "border-b-2 border-[#2a9d8f] text-[#2a9d8f]",
-                "hover:border-b-2 hover:border-[#2a9d8f] transition duration-500",
-
+                pathname == link.path &&
+                  "border-b-2 border-[#2a9d8f] text-[#2a9d8f]",
+                "hover:border-b-2 hover:border-[#2a9d8f] transition duration-500"
               )}
             >
               {link.name}
@@ -28,6 +34,23 @@ export default function Navbar() {
           );
         })}
       </ul>
+      <div className="flex gap-5">
+        {theme === "light" ? (
+          <Sun
+            size={25}
+            className="cursor-pointer"
+            color="gray"
+            onClick={() => setTheme()}
+          />
+        ) : (
+          <Moon
+            size={25}
+            className="cursor-pointer"
+            color="gray"
+            onClick={() => setTheme()}
+          />
+        )}
+      </div>
     </div>
   );
 }
