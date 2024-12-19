6389118f67e08ed30eecb9a7bdb2126fb26022b3
INGENIO <ingeniokhalif@gmail.com>
2024-12-19 14:02:28 +0100

    updated skills, projects and cv

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,15 +11,15 @@ export default function Page() {
       title: "Toolbox",
       roles: ["Project Lead", "Developer"],
       description:
-        "A multi-region payment aggregator for seamless integration of online payments(Stripe & Mobile payments) and SMS(Twilio) sending features, that aims to reduce internal projects development time, and increase developers' productivity.",
+        "A toolkit for seamless multi-region payment and sms sending integrations built on top of Stripe, Pawapay and Twilio",
       link: "https://shorturl.at/jnLGo",
     },
     {
-      title: "First Event",
-      roles: ["Project Lead", "Developer"],
+      title: "Cameroon E-Sport",
+      roles: ["Developer"],
       description:
-        "Events management and ticketing platform based in Canada. The service enables users to search for, create and promote local events.",
-      link: "https://link.dev",
+        "A platform requested by the Cameroon E-Sport Federation, that will help them recruit and manage Gamers all around the country",
+      link: "https://shorturl.at/xN7CR",
     },
     {
       title: "Latrice",
@@ -29,39 +29,11 @@ export default function Page() {
       link: "https://shorturl.at/e5oQf",
     },
     {
-      title: "YaFoot-FC",
+      title: "RACING FC",
       roles: ["Developer"],
       description:
-        "A web platform for managing the internal activities of the Yaounde II FC (YaFoot). Supporters can buy member's cards, articles(jerseys, goodies, etc...), and make donations to the club.",
-      link: "https://shorturl.at/q9fGA",
-    },
-    {
-      title: "Pokedex",
-      roles: ["Developer"],
-      description:
-        "A (hobby) mobile app that takes advantage of the pokeApi, to display a list of pokemons, filter/sort them, and get details on a given pokemon.",
-      link: "https://link.dev",
-    },
-    {
-      title: "MYWAY Voyage",
-      roles: ["Developer"],
-      description:
-        "Website of the travelling agency MYWAY Voyage. It presents the different services offered by the agency, such as: Foreign study visa, Foreign work visa, etc...",
-      link: "https://mw-voyage.vercel.app/",
-    },
-    {
-      title: "Evenafro",
-      roles: ["Developer"],
-      description:
-        "Events mobile platform based in Canada that aims to promote Black-African culture by allowing its users browse for local events.",
-      link: "https://evenafro.com",
-    },
-    {
-      title: "TV Box",
-      roles: ["Developer"],
-      description:
-        "Website of a merchant based in Canada that sells TV Boxes. The website aims to presents its products and their features.",
-      link: "https://link.dev",
+        "A web platform for managing the internal activities of the RACING FC of  Bafoussam. Supporters can buy member's cards, articles(jerseys, goodies, etc...), and make donations to the club.",
+      link: "https://shorturl.at/qSGKC",
     },
     {
       title: "IME-SCHOOL",
@@ -70,6 +42,48 @@ export default function Page() {
         "Website of IME-SCHOOL, it presents the different programs and fields of study present there.",
       link: "https://www.ime-school.com/",
     },
+    {
+      title: "First Event",
+      roles: ["Project Lead", "Developer"],
+      description:
+        "Events management platform based in Canada for creating, managing and promoting local events",
+      // link: "https://link.dev",
+    },
+    // {
+    //   title: "YaFoot-FC",
+    //   roles: ["Developer"],
+    //   description:
+    //     "A web platform for managing the internal activities of the Yaounde II FC (YaFoot). Supporters can buy member's cards, articles(jerseys, goodies, etc...), and make donations to the club.",
+    //   link: "https://shorturl.at/q9fGA",
+    // },
+    // {
+    //   title: "Pokedex",
+    //   roles: ["Developer"],
+    //   description:
+    //     "A (hobby) mobile app that takes advantage of the pokeApi, to display a list of pokemons, filter/sort them, and get details on a given pokemon.",
+    //   link: "https://link.dev",
+    // },
+    // {
+    //   title: "MYWAY Voyage",
+    //   roles: ["Developer"],
+    //   description:
+    //     "Website of the travelling agency MYWAY Voyage. It presents the different services offered by the agency, such as: Foreign study visa, Foreign work visa, etc...",
+    //   link: "https://mw-voyage.vercel.app/",
+    // },
+    // {
+    //   title: "Evenafro",
+    //   roles: ["Developer"],
+    //   description:
+    //     "Events mobile platform based in Canada that aims to promote Black-African culture by allowing its users browse for local events.",
+    //   link: "https://evenafro.com",
+    // },
+    // {
+    //   title: "TV Box",
+    //   roles: ["Developer"],
+    //   description:
+    //     "Website of a merchant based in Canada that sells TV Boxes. The website aims to presents its products and their features.",
+    //   link: "https://link.dev",
+    // },
   ];
 
   return (
@@ -96,13 +110,15 @@ export default function Page() {
           >
             <h2 className="font-bold text-2xl">{project.title}</h2>
             <p className="text-md">{project.description}</p>
-            <Link
-              href={project.link}
-              target="_blank"
-              className="text-p_green  hover:underline flex items-center gap-2 flex-1/3"
-            >
-              <LinkIcon size={15} /> <h1>{project.link}</h1>
-            </Link>
+            {project.link && (
+              <Link
+                href={project.link}
+                target="_blank"
+                className="text-p_green  hover:underline flex items-center gap-2 flex-1/3"
+              >
+                <LinkIcon size={15} /> <h1>{project.link}</h1>
+              </Link>
+            )}
           </motion.div>
         ))}
       </div>
